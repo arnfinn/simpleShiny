20f9e75d3c6ca4fd9b0a183fcd9287c59832fe4b
arnfinn <arnfinn.steindal@gmail.com>
2019-04-26 18:18:56 +0200

    rm shiny part

--- inst/app/tests/mytest.R
+++
@@ -1,7 +0,0 @@
-app <- ShinyDriver$new("../")
-app$snapshotInit("mytest")
-
-app$setInputs(bins = 20)
-app$snapshot()
-app$setInputs(bins = 10)
-app$snapshot()
--- tests/testthat/test_app.R
+++
@@ -1,9 +0,0 @@
-context("Test shiny app")
-
-library(shinytest)
-
-test_that("app gets expected output", {
-  testthat::skip_on_cran()
-  appdir <- system.file(package = "simpleShiny", "app")
-  expect_pass(testApp(appdir, compareImages = FALSE))
-})
